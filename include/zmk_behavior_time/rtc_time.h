26223390ae7bf16e846b25d1241af66603467aff
Jonas Fuhrmann <jofrfu@googlemail.com>
2025-08-30 15:27:17 +0200

    Fix

--- include/zmk_behavior_time/rtc_time.h
+++ include/zmk_behavior_time/rtc_time.h
@@ -12,11 +12,11 @@ struct rtc_state {
     int64_t uptime_ref;
 };
 
-extern struct rtc_state rtc = {0};
+extern struct rtc_state rtc;
 
 #define MAX_TIME_DIGITS 12 // YYYYMMDDHHMM
 
 extern char time_input_buffer[MAX_TIME_DIGITS + 1];
-extern int time_input_len = 0;
+extern int time_input_len;
 
 void rtc_get_time(struct rtc_state *out);
